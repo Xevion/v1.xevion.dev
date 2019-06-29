701dddab32516aa7d2756f7795d9bc234d1883f7
Xevion <ryanchwalters@gmail.com>
2019-06-29 16:34:38 -0500

    changed config file for security

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+with open('key', 'r') as key:
+	key = key.read()
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '641e30802818e6bc77d11f428675525fcbff1c22eaf1dac17525edefb4d84899'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or key
     TEMPLATES_AUTO_RELOAD=True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
